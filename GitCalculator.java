a5e25ab412c5de849db6e51033dcbcb44d6d0041
Ruthiimi <perisruthnyakio@gmail.com>
2017-01-20 17:33:24 +0000

    Add is working

---
+++ GitCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+
+public class GitCalculator {
+  public static void main (String [] args) { 
+    Scanner input = new Scanner(System.in);
+    
+    int firstNum=0, secNum=0, ans=0 ; 
+    for(int i=0;i<args.length;i++)
+    {
+      if(args[1].equalsIgnoreCase("+"))
+      {
+        System.out.println(Integer.parseInt(args[0])+Integer.parseInt(args[2]));
+      }
+      break;
+    }
+  }
+  
+}
