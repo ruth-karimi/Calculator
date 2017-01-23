041c1eeff69752afcd195983da27abacf670ea78
alieu <alieujallow93@gmail.com>
2017-01-23 22:35:59 +0000

    divide

--- GitCalculator.java
+++ GitCalculator.java
@@ -18,6 +18,12 @@ public class GitCalculator {
       {
         System.out.println(Integer.parseInt(args[0]) * Integer.parseInt(args[2]));
       }
+	
+      if(args[1].equalsIgnoreCase("/"))
+      {
+        System.out.println(Integer.parseInt(args[0]) / Integer.parseInt(args[2]));
+      }
+
       break;
     }
   }
